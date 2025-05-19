e27e901dc9584eb2382f72a48b214bc484c75cd9
caleb <calebtelfe@gmail.com>
2025-05-19 18:14:01 -0400

    moved majority of init html to home-page.js

--- src/home-page.js
+++ src/home-page.js
@@ -1,5 +1,45 @@
 import {logo} from "./logo.svg";
 
-function loadHomePage() {
+export function loadHomePage() {
     const content = document.getElementById("content");
+    const nav = document.querySelector("nav");
+
+    while (nav.firstChild) {
+        nav.removeChild(nav.firstChild);
+    }
+
+    while (content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
+
+
+    const viewProjects = document.createElement("h2");
+    const createNewProject = document.createElement("h2");
+    const deleteAProject = document.createElement("h2");
+
+    viewProjects.textContent = "View Projects";
+    createNewProject.textContent = "Create a Project";
+    deleteAProject.textContent = "Delete a Project";
+
+    viewProjects.style.cursor = "pointer";
+    createNewProject.style.cursor = "pointer";
+    deleteAProject.style.cursor = "pointer";
+
+    viewProjects.addEventListener("click", function() {
+
+    })
+
+    createNewProject.addEventListener("click", function() {
+        
+    })
+
+    deleteAProject.addEventListener("click", function() {
+        
+    })
+
+
+
+    nav.appendChild(viewProjects);
+    nav.appendChild(createNewProject);
+    nav.appendChild(deleteAProject);
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./styles.css";
+import { loadHomePage } from "./home-page";
+
+loadHomePage();
